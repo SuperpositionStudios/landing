07af13403dcf9fedc1630e2f226c5fdc33ab3008
caleb <caleb.wentworth@baxter-academy.org>
2017-05-31 13:26:20 -0400

    Customized 404 Page

--- site/src/components/Error404.vue
+++ site/src/components/Error404.vue
@@ -1,25 +1,9 @@
 <template>
-    <div class="error-page window-height window-width bg-light column items-center">
-        <div class="error-code bg-primary flex items-center justify-center">
-            404
-        </div>
-        <div>
-            <div class="error-card card bg-white column items-center justify-center">
-                <i class="text-grey-5">error_outline</i>
-                <p class="caption text-center">Oops. Nothing here...</p>
-                <p class="text-center group">
-                    <button v-if="canGoBack" class="grey push small" @click="goBack">
-                        <i class="on-left">keyboard_arrow_left</i>
-                        Go back
-                    </button>
-                    <router-link to="/">
-                        <button class="grey push small">
-                            Go home
-                            <i class="on-right">home</i>
-                        </button>
-                    </router-link>
-                </p>
-            </div>
+    <div class="layout-view">
+        <div class="layout-padding">
+            <h1 class="text-white text-center">Oh no!</h1>
+            <p class="text-white text-center">You fell into the blackhole, yo</p>
+            <button class="clear text-white full-width" @click="goBack">Go Back</button>
         </div>
     </div>
 </template>
@@ -27,9 +11,7 @@
 <script>
     export default {
         data () {
-            return {
-                canGoBack: window.history.length > 1
-            }
+            return {}
         },
         methods: {
             goBack () {
@@ -40,19 +22,20 @@
 </script>
 
 <style lang="stylus">
-    .error-page
-        .error-code
-            height 50vh
-            width 100%
-            padding-top 15vh
-            font-size 30vmax
-            color rgba(255, 255, 255, .2)
-            overflow hidden
-        .error-card
-            margin-top -25px
-            width 90vw
-            max-width 600px
-            padding 50px
-            i
-                font-size 5rem
+    body {
+        background-image: url('../assets/404.jpg');
+        background-repeat: no-repeat;
+        background-position: center;
+    }
+
+    h1 {
+        margin-top: 10%;
+    }
+
+    button {
+        margin-top: 20%
+        border-style: solid;
+        border-color: #FFFFFF;
+        border-width: 2px;
+    }
 </style>
